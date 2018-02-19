9f88137239e25915ddba3f1c6bdadb27a38bae8e
Elaine Choo <elainechoo85@gmail.com>
2018-02-19 22:21:21 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,25 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped Queen");
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped King");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.")
+var checkForMatch = function (){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
 	}
 }
-else {
-	console.log("Please flip another card.")
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 }
+
+flipCard (0);
+flipCard (2);
+
+
